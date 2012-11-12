9c169dbe1bbca05c622a1183968a27e92886bf8b
robin <robin@robin-HP-Pavilion-dm4-Notebook-PC>
2012-11-12 15:12:06 +0100

    Projet Symfony Anaphore

---
+++ git@dev.anaphore.eu:bach.git/Anaphore/app/cache/dev/twig/7e/18/aa6bf73002335f3cfdceb2e4b42f.php
@@ -0,0 +1,48 @@
+<?php
+
+/* AnphIndexationBundle:Indexation:index.html.twig */
+class __TwigTemplate_7e18aa6bf73002335f3cfdceb2e4b42f extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 2
+        echo "
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Anaphore - Indexation</title>
+    </head>
+    <body>
+        <h1>Hello World !</h1>
+
+        <p>
+            Le Hello World est un grand classique en programmation.
+            Il signifie énormément, car cela veut dire que vous avez
+            réussi à exécuter le programme pour accomplir une tâche simple :
+            afficher ce hello world !
+        </p>
+    </body>
+</html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "AnphIndexationBundle:Indexation:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  17 => 2,);
+    }
+}
