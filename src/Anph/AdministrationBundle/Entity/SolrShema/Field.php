6c8a444048417b1207a2fbeb2359b70c664d8be9
Yassine <Yassine.el-hajri@esial.net>
2012-11-15 17:40:32 +0100

    Maj Field

--- src/Anph/AdministrationBundle/Entity/SolrShema/Field.php
+++ src/Anph/AdministrationBundle/Entity/SolrShema/Field.php
@@ -2,58 +2,88 @@
 namespace Anph\AdministrationBundle\Entity\SolrShema;
 
 class Field {
+	
+	private $name;
+	private $type;
 	private $indexed;
 	private $stored;
-	private $compressed;
-	private $compressThreshold;
 	private $multiValued;
+	private $required;
+	private $defaut;
 	private $omitNorms;
-	private $termVectors;
 	private $omitTermFreqAndPositions;
 	private $omitPosition;
+	private $termVectors;
+	private $termPositions;
+	private $termOffsets;
 	
-	function __construct($indexed,$stored,$compressed,$compressThreshold, $multivalued, $omitNorms, $termVectors,$omitTermFreqAndPositions,$omitPosition ){
-		this->$indexed = $indexed;
-		this->$stored = $stored;
-		this->$compressed= $compressed;
-		this->$compressThreshold= $compressThreshold;
-		this->$multiValued=  $multiValued;
-		this->$omitNorms= $omitNorms;
-		this->$termVectors= $termVectors;
-		this->$omitTermFreqAndPositions = $omitTermFreqAndPositions;
-		this->$omitPosition= $omitPosition;	
+
+	
+	function __construct($name, $type, $indexed, $stored, $multiValued, $required, $defaut, $omitNorms,$omitTermFreqAndPositions,$omitPosition,$termVectors,$termPositions, $termOffsets ){
+		$this->name=$name;
+		$this->type=$type;
+		$this->indexed = $indexed;
+		$this->stored = $stored;
+		$this->multiValued=  $multiValued;
+		$this->required=$required;
+		$this->defaut=$defaut;
+		$this->omitNorms= $omitNorms;
+		$this->omitTermFreqAndPositions = $omitTermFreqAndPositions;
+		$this->omitPosition= $omitPosition;	
+		$this->termVectors= $termVectors;
+		$this->termPositions=$termPositions;
+		$this->termOffsets=$termOffsets;
+		
+	}
+	
+	private function getName(){
+		return $this->name;
+	}
+
+	private function getType(){
+		return $this->type;
 	}
 	
 	private function isIndexed(){
-		return this->$indexed;
+		return $this->indexed;
 	}
 	
 	private function isStored(){
-		return this->$stored;
+		return $this->stored;
 	}
-	
-	private function isCompressed(){
-		return this->$compressed;
-	}
-	
-	private function isCompressThreshold(){
-		return this->$compressThreshold;
-	}
+
 	
 	private function isMultiValued(){
-		return this->$multiValued;
+		return $this->multiValued;
 	}
 	
-	private function isStored(){
-		return this->$stored;
+	private function isRequired(){
+		return $this->required;
 	}
 	
-	private function isStored(){
-		return this->$stored;
+	private function isOmitNorms(){
+		return $this->omitNorms;
 	}
 	
+	private function isOmitPosition(){
+		return $this->omitPosition;
+	}
 	
+	private function isOmitTermFreqAndPositions(){
+		return $this->omitTermFreqAndPositions;
+	}
 	
+ 	private function isTermOffsets(){
+		return $this->termOffsets;
+	}
+	
+	private function isTermPositions(){
+		return $this->termPositions;
+	}
+	
+	private function isTermVectors(){
+		return $this->termVectors;
+	}
 	
 	
 
