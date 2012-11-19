bb8ac51f14e6fa5c28d04cf4add8f4031f3a365c
yassine <Yassine.el-hajri@esial.net>
2012-11-19 17:36:54 +0100

    Ajout des méthodes Fields

--- src/Anph/AdministrationBundle/Entity/SolrShema/Fields.php
+++ src/Anph/AdministrationBundle/Entity/SolrShema/Fields.php
@@ -5,6 +5,51 @@ class Fields {
 	private $dynamicFieldList;
 	
 	public function __construct(){
+		$this->fieldList = array();
+		$this->dynamicFieldList=array();
+	}
+	
+	
+	private function getFieldList(){
+		return $this->fieldList;
+	}
+	
+	private function getDynamicFieldList(){
+		return $this->dynamicFieldList;
+	}
+	
+	private function getFieldAtIndex($index){
+		return $this->fieldList[$index];
+	}
+	
+	private function getDynamicFieldAtIndex($index){
+		return $this->dynamicFieldList[$index];
+	}
+	
+	
+	private function addStaticField($staticField){
+		$this->fieldList=$staticField;
+	}
+	
+	private function addDynamicField($dynamicField){
+	 	$this->dynamicFieldList=$dynamicField;
+	}
+	
+	private function resetLists(){
+		$this->fieldList=NULL;
+		$this->dynamicFieldList=NULL;
+		$this->fieldList=array();
+		$this->dynamicFieldList=array();
+	}
+	
+	private function setStaticFieldList($newStaticFieldList){
+		$this->fieldList=$newStaticFieldList;
 		
 	}
+	
+	
+	
+	
+	
+	
 }
