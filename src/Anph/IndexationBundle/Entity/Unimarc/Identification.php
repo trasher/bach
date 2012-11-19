485f3a3a3c73663dfa4f1102ffc7fda63610a79a
forest <jocelyn.forest@esial.net>
2012-11-19 12:43:43 +0100

    Indexation Unimarc

---
+++ src/Anph/IndexationBundle/Entity/Unimarc/Identification.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Anph\IndexationBundle\Entity\Unimarc;
+
+class Identification 
+{
+	//private $num = array{};
+	private $numCodes;
+	
+	public function __construct($blocIdent, $blocInfo) {
+		$size = strlen($blocIdent);
+		$indice = 0;
+		$numCodes = array();
+		for ($i = 0; $i <= $size; $i++) {
+			$this->numCodes[$i] = substr($blocIdent,$indice,11);
+			$indice = $indice + 12;
+			
+		}
+		echo ("test" .$numCodes[0]);
+		
+	}
+	
+	
+}
+
+?>
