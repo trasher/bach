b3e20a64fb94c423e12a3a3ab55a23499edeffa5
rems <rems@remsPC>
2012-11-21 15:05:01 +0100

    Premier Test d'essai

--- src/Anph/IndexationBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Anph/IndexationBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,15 +3,14 @@
 namespace Anph\IndexationBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Finder\SplFileInfo;
 
 class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $fileInfo = new SplFileInfo("FRAD027_404142R.xml",__DIR__.'/../../Controller/FRAD027_404142R.xml',__DIR__);
+    	
+        $this->assertTrue(file_exists($fileInfo->getRelativePath()));
     }
 }
