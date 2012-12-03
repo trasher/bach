c0ede1c65b62dfddde6c23f5b84b9209747790f0
robin <robin@robin-HP-Pavilion-dm4-Notebook-PC>
2012-12-03 14:56:43 +0100

    Utilisation du SplFileInfo de PHP et non de Symfony

--- src/Anph/IndexationBundle/Entity/Bag/TextDataBag.php
+++ src/Anph/IndexationBundle/Entity/Bag/TextDataBag.php
@@ -2,7 +2,6 @@
 
 namespace Anph\IndexationBundle\Entity\Bag;
 
-use Symfony\Component\Finder\SplFileInfo;
 use Anph\IndexationBundle\Entity\DataBag;
 
 class TextDataBag extends DataBag
@@ -11,7 +10,7 @@ class TextDataBag extends DataBag
 	* The constructor
 	* @param SplFileInfo $fileInfo The input file
 	*/
-	public function __construct(SplFileInfo $fileInfo)
+	public function __construct(\SplFileInfo $fileInfo)
 	{
 		$this->type = "txt";
 		$this->data = file_get_contents($fileInfo->getRealPath());
