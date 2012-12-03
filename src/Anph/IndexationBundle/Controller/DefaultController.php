9a24af70e205c172facd64f77215019e30e2c149
robin <robin@robin-HP-Pavilion-dm4-Notebook-PC>
2012-12-03 17:32:50 +0100

    Mise à jour gestionnaire d'adaptateurs, prise en charge de multiple universalfileformat

--- src/Anph/IndexationBundle/Controller/DefaultController.php
+++ src/Anph/IndexationBundle/Controller/DefaultController.php
@@ -12,7 +12,7 @@ class DefaultController extends Controller
     {
     	$manager = $this->container->get('anph_indexation.file_driver_manager');
     	$factory = $this->container->get('anph_indexation.data_bag_factory'); // Fourni le bon DataBag pour le fichier à indexer
-    	
+    
     	//$fileInfo = new \SplFileInfo(__DIR__.'/FRAD027_PC.xml');
     	$fileInfo = new \SplFileInfo(__DIR__.'/FRAD027_404142R.xml');
     	//$fileInfo = new \SplFileInfo(__DIR__.'/fsor2709.c01');
--- src/Anph/IndexationBundle/Entity/UniversalFileFormat.php
+++ src/Anph/IndexationBundle/Entity/UniversalFileFormat.php
@@ -4,9 +4,46 @@ namespace Anph\IndexationBundle\Entity;
 
 use Symfony\Component\Yaml\Yaml;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+* @ORM\Entity
+* @ORM\Table(name="UniversalFileFormat")
+*/
 class UniversalFileFormat
 {
 	private $configuration = array();
+	
+	/**
+	* @ORM\Id
+	* @ORM\Column(type="string", length=100)
+	*/
+	private $headerId;
+	
+	/**
+	* @ORM\Column(type="string", length=100)
+	*/
+	private $headerAuthor;
+	
+	/**
+	* @ORM\Column(type="date")
+	*/
+	private $headerDate;
+	
+	/**
+	* @ORM\Column(type="string", length=100)
+	*/
+	private $headerPublisher;
+	
+	/**
+	* @ORM\Column(type="text")
+	*/
+	private $headerAddress;
+	
+	/**
+	* @ORM\Column(type="string", length=3)
+	*/
+	private $headerLanguage;
 	
 	/**
 	* The constructor
@@ -15,16 +52,16 @@ class UniversalFileFormat
     public function __construct($data)
     {
     	$this->importConfiguration();
-    	$this->parseData($data);
+    	$this->parseData($data,$this->configuration);
     }
     
-    private function parseData($data, $keys = array())
+    private function parseData($data, $configuration, $keys = array())
     {
-    	foreach ($this->configuration as $key=>$config) {
+    	foreach ($configuration as $key=>$config) {
     		if (is_array($config)) {
-    			$this->parseData($data,array_merge($keys,array($key)));
+    			$this->parseData(array($key=>$data[$key]),$config,array_merge($keys,array($key)));
     		} else {
-    			$method = "set".implode(array_map('ucfirst',$keys));
+    			$method = "set".implode(array_map('ucfirst',$keys)).ucfirst($config);
     			$this->$method($this->lookForKeys($keys,$data));
     		}
     	}
