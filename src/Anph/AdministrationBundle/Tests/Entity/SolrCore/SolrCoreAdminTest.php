af9bbc8ed6a3ece737d4cfe0a94c59604430cb71
Philippe Kalitine <phillippe.kalitine@esial.net>
2012-12-12 16:22:12 +0100

    Last some modifications to add to SolcCore part

--- src/Anph/AdministrationBundle/Tests/Entity/SolrCore/SolrCoreAdminTest.php
+++ src/Anph/AdministrationBundle/Tests/Entity/SolrCore/SolrCoreAdminTest.php
@@ -4,19 +4,52 @@ use Anph\AdministrationBundle\Entity\SolrCore\SolrCoreAdmin;
 
 class SolrCoreAdminTest extends \PHPUnit_Framework_TestCase
 {
-	private $sca;
-	
-	public function setUp() {
-		$this->sca = new SolrCoreAdmin();
-	}
-	
-	public function tearDown() {
-		unset($this->sca);
-	}
-	
-	public function testCreate() {
-		$response = $this->sca->create('bobo');
-		echo '##########' . $response . '#########';
-		$this->assertTrue($response !== false);
-	}
+    private $sca;
+
+    public function setUp() {
+        $this->sca = new SolrCoreAdmin();
+    }
+
+    public function tearDown() {
+        unset($this->sca);
+    }
+
+    public function testCreate() {
+        $response = $this->sca->create('core2');
+        if ($response === true) {
+            $this->assertTrue(true);
+        } else {
+            echo "###BAAM###";
+            echo 'Error message : ' . $response->getMessage();
+            echo 'Trace : ' . $response->getTrace() !== null ? $response->getTrace() : 'NULL';
+            echo 'Code : ' .$response->getCode();
+            echo "###BOOM###";
+            $this->assertTrue(false);
+        }   
+    }
+   /* 
+    public function testGetStatusAllCore() {
+        $response = $this->sca->getStatus();
+        echo 'STATUS ALL : ###' . $response . '###';
+        $this->assertTrue(true);
+    }
+    
+    public function testGetStatusOneCore() {
+        $response = $this->sca->getStatus('core0');
+        echo 'STATUS ONE : ###' . $response . '###';
+        $this->assertTrue(true);
+    }
+    
+    public function testReload() {
+        $response = $this->sca->reload('core0');
+        echo 'RELOAD : ###' . $response . '###';
+        $this->assertTrue(true);
+    }
+    
+    public function testRename() {
+        $response = $this->sca->create('coreTest');
+        $response = $this->sca->rename('coreTest', 'coreNewTest');
+        echo 'RENAME : ###' . $response . '###';
+        $this->assertTrue(true);
+    }*/
 }
