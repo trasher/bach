470d121e5715df4eb9bc62f65851aedf95351d34
rem's <rems@remsPC.(none)>
2013-01-21 16:06:39 +0100

    Mise à jour js Uploader

---
+++ src/Anph/IndexationBundle/Entity/UniversalFileFormat/UNIMARCFileFormat.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace Anph\IndexationBundle\Entity\UniversalFileFormat;
+
+use Anph\IndexationBundle\Entity\UniversalFileFormat;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+* @ORM\Entity
+* @ORM\Table(name="UNIMARCUniversalFileFormat")
+*/
+class UNIMARCFileFormat extends UniversalFileFormat
+{
+	protected $accessRestrict;
+}
+?>
