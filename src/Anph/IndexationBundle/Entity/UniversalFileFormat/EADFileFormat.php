9450f4c994f8a63c7c3cd66d4db229bdc73a2825
forest <jocelyn.fores@esial.net>
2013-01-21 15:37:12 +0100

    mappin des champs ead

--- src/Anph/IndexationBundle/Entity/UniversalFileFormat/EADFileFormat.php
+++ src/Anph/IndexationBundle/Entity/UniversalFileFormat/EADFileFormat.php
@@ -13,5 +13,28 @@ use Doctrine\ORM\Mapping as ORM;
 class EADFileFormat extends UniversalFileFormat
 {
 	protected $accessRestrict;
+	protected $abbr;
+	protected $abstract;
+	protected $accruals;
+	protected $acqinfo;
+	protected $address;
+	protected $altformavail;
+	protected $appraisal;
+	protected $archref;
+	protected $arrangement;
+	protected $author;
+	protected $bibliography;
+	protected $bibref;
+	protected $bibseries;
+	protected $bioghist;
+	protected $container;
+	protected $controlaccess;
+	protected $custodhist;
+	protected $date;
+	protected $daodesc;
+	
+	
+	
+	
 }
 ?>
