3c71cd877e746daf12082469dc999b12ef42041b
rems <rems@remsPC>
2013-01-21 17:13:31 +0100

    Mapping Suite + Début nouveau upload

---
+++ src/Anph/IndexationBundle/Entity/Document.php
@@ -0,0 +1,56 @@
+<?php
+/*
+* This file is part of the bach project.
+*/
+
+namespace Anph\IndexationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ */
+class Document
+{
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	public $id;
+
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 * @Assert\NotBlank
+	 */
+	public $name;
+
+	/**
+	 * @ORM\Column(type="string", length=255, nullable=true)
+	 */
+	public $path;
+
+	public function getAbsolutePath()
+	{
+		return null === $this->path ? null : $this->getUploadRootDir().'/'.$this->path;
+	}
+
+	public function getWebPath()
+	{
+		return null === $this->path ? null : $this->getUploadDir().'/'.$this->path;
+	}
+
+	protected function getUploadRootDir()
+	{
+		// le chemin absolu du répertoire où les documents uploadés doivent être sauvegardés
+		return __DIR__.'/../../../../web/'.$this->getUploadDir();
+	}
+
+	protected function getUploadDir()
+	{
+		// on se débarrasse de « __DIR__ » afin de ne pas avoir de problème lorsqu'on affiche
+		// le document/image dans la vue.
+		return 'uploads/documents';
+	}
+}
--- src/Anph/IndexationBundle/Entity/UniversalFileFormat/EADFileFormat.php
+++ src/Anph/IndexationBundle/Entity/UniversalFileFormat/EADFileFormat.php
@@ -1,42 +1,212 @@
-<?php 
+<?php
 
 namespace Anph\IndexationBundle\Entity\UniversalFileFormat;
-
 use Anph\IndexationBundle\Entity\UniversalFileFormat;
 
 use Doctrine\ORM\Mapping as ORM;
 
 /**
-* @ORM\Entity
-* @ORM\Table(name="EADUniversalFileFormat")
-*/
-class EADFileFormat extends UniversalFileFormat
-{
-	protected $archeDescRootUnitid,
-                    $archeDescRootUnittitle,
-                    $archeDescRootUnitdate,
-                    $archeDescRootPhysdesc,
-                    $archeDescRootRepository,
-                    $archeDescRootLangmaterial,
-                   	$archeDescRootOrigination,
-                    $archeDescRootAcqinfo,
-                    $archeDescRootScopecontent,
-                    $archeDescRootAccruals,
-                    $archeDescRootArrangement,
-                    $archeDescRootAccessrestrict,
-                    $archeDescRootLegalstatus,
-                    $archeDescRootUserestrict,
-                    $archeDescRootOriginalsloc,
-                    $archeDescRootRelatedmaterial,
-                    $archeDescRootOdd,
-                    $archeDescRootProcessinfo,
-                    $archeDescRootControlaccess;
-	
-	protected $cUnitid,
-                    $cUnittitle,
-                    $cCopcontent,
-                    $cControlacces,
-                    $cDaoloc;
-	
+ * @ORM\Entity
+ * @ORM\Table(name="EADUniversalFileFormat")
+ */
+class EADFileFormat extends UniversalFileFormat {
+	protected $archeDescRootUnitid, $archeDescRootUnittitle, $archeDescRootUnitdate, $archeDescRootPhysdesc, $archeDescRootRepository, $archeDescRootLangmaterial, $archeDescRootOrigination, $archeDescRootAcqinfo, $archeDescRootScopecontent, $archeDescRootAccruals, $archeDescRootArrangement, $archeDescRootAccessrestrict, $archeDescRootLegalstatus, $archeDescRootUserestrict, $archeDescRootOriginalsloc, $archeDescRootRelatedmaterial, $archeDescRootOdd, $archeDescRootProcessinfo, $archeDescRootControlaccess;
+
+	protected $cUnitid, $cUnittitle, $cCopcontent, $cControlacces, $cDaoloc;
+
+	public function getArcheDescRootUnitid() {
+		return $this->archeDescRootUnitid;
+	}
+
+	public function setArcheDescRootUnitid($archeDescRootUnitid) {
+		$this->archeDescRootUnitid = $archeDescRootUnitid;
+	}
+
+	public function getArcheDescRootUnittitle() {
+		return $this->archeDescRootUnittitle;
+	}
+
+	public function setArcheDescRootUnittitle($archeDescRootUnittitle) {
+		$this->archeDescRootUnittitle = $archeDescRootUnittitle;
+	}
+
+	public function getArcheDescRootUnitdate() {
+		return $this->archeDescRootUnitdate;
+	}
+
+	public function setArcheDescRootUnitdate($archeDescRootUnitdate) {
+		$this->archeDescRootUnitdate = $archeDescRootUnitdate;
+	}
+
+	public function getArcheDescRootPhysdesc() {
+		return $this->archeDescRootPhysdesc;
+	}
+
+	public function setArcheDescRootPhysdesc($archeDescRootPhysdesc) {
+		$this->archeDescRootPhysdesc = $archeDescRootPhysdesc;
+	}
+
+	public function getArcheDescRootRepository() {
+		return $this->archeDescRootRepository;
+	}
+
+	public function setArcheDescRootRepository($archeDescRootRepository) {
+		$this->archeDescRootRepository = $archeDescRootRepository;
+	}
+
+	public function getArcheDescRootLangmaterial() {
+		return $this->archeDescRootLangmaterial;
+	}
+
+	public function setArcheDescRootLangmaterial($archeDescRootLangmaterial) {
+		$this->archeDescRootLangmaterial = $archeDescRootLangmaterial;
+	}
+
+	public function getArcheDescRootOrigination() {
+		return $this->archeDescRootOrigination;
+	}
+
+	public function setArcheDescRootOrigination($archeDescRootOrigination) {
+		$this->archeDescRootOrigination = $archeDescRootOrigination;
+	}
+
+	public function getArcheDescRootAcqinfo() {
+		return $this->archeDescRootAcqinfo;
+	}
+
+	public function setArcheDescRootAcqinfo($archeDescRootAcqinfo) {
+		$this->archeDescRootAcqinfo = $archeDescRootAcqinfo;
+	}
+
+	public function getArcheDescRootScopecontent() {
+		return $this->archeDescRootScopecontent;
+	}
+
+	public function setArcheDescRootScopecontent($archeDescRootScopecontent) {
+		$this->archeDescRootScopecontent = $archeDescRootScopecontent;
+	}
+
+	public function getArcheDescRootAccruals() {
+		return $this->archeDescRootAccruals;
+	}
+
+	public function setArcheDescRootAccruals($archeDescRootAccruals) {
+		$this->archeDescRootAccruals = $archeDescRootAccruals;
+	}
+
+	public function getArcheDescRootArrangement() {
+		return $this->archeDescRootArrangement;
+	}
+
+	public function setArcheDescRootArrangement($archeDescRootArrangement) {
+		$this->archeDescRootArrangement = $archeDescRootArrangement;
+	}
+
+	public function getArcheDescRootAccessrestrict() {
+		return $this->archeDescRootAccessrestrict;
+	}
+
+	public function setArcheDescRootAccessrestrict(
+			$archeDescRootAccessrestrict) {
+		$this->archeDescRootAccessrestrict = $archeDescRootAccessrestrict;
+	}
+
+	public function getArcheDescRootLegalstatus() {
+		return $this->archeDescRootLegalstatus;
+	}
+
+	public function setArcheDescRootLegalstatus($archeDescRootLegalstatus) {
+		$this->archeDescRootLegalstatus = $archeDescRootLegalstatus;
+	}
+
+	public function getArcheDescRootUserestrict() {
+		return $this->archeDescRootUserestrict;
+	}
+
+	public function setArcheDescRootUserestrict($archeDescRootUserestrict) {
+		$this->archeDescRootUserestrict = $archeDescRootUserestrict;
+	}
+
+	public function getArcheDescRootOriginalsloc() {
+		return $this->archeDescRootOriginalsloc;
+	}
+
+	public function setArcheDescRootOriginalsloc($archeDescRootOriginalsloc) {
+		$this->archeDescRootOriginalsloc = $archeDescRootOriginalsloc;
+	}
+
+	public function getArcheDescRootRelatedmaterial() {
+		return $this->archeDescRootRelatedmaterial;
+	}
+
+	public function setArcheDescRootRelatedmaterial(
+			$archeDescRootRelatedmaterial) {
+		$this->archeDescRootRelatedmaterial = $archeDescRootRelatedmaterial;
+	}
+
+	public function getArcheDescRootOdd() {
+		return $this->archeDescRootOdd;
+	}
+
+	public function setArcheDescRootOdd($archeDescRootOdd) {
+		$this->archeDescRootOdd = $archeDescRootOdd;
+	}
+
+	public function getArcheDescRootProcessinfo() {
+		return $this->archeDescRootProcessinfo;
+	}
+
+	public function setArcheDescRootProcessinfo($archeDescRootProcessinfo) {
+		$this->archeDescRootProcessinfo = $archeDescRootProcessinfo;
+	}
+
+	public function getArcheDescRootControlaccess() {
+		return $this->archeDescRootControlaccess;
+	}
+
+	public function setArcheDescRootControlaccess($archeDescRootControlaccess) {
+		$this->archeDescRootControlaccess = $archeDescRootControlaccess;
+	}
+
+	public function getCUnitid() {
+		return $this->cUnitid;
+	}
+
+	public function setCUnitid($cUnitid) {
+		$this->cUnitid = $cUnitid;
+	}
+
+	public function getCUnittitle() {
+		return $this->cUnittitle;
+	}
+
+	public function setCUnittitle($cUnittitle) {
+		$this->cUnittitle = $cUnittitle;
+	}
+
+	public function getCCopcontent() {
+		return $this->cCopcontent;
+	}
+
+	public function setCCopcontent($cCopcontent) {
+		$this->cCopcontent = $cCopcontent;
+	}
+
+	public function getCControlacces() {
+		return $this->cControlacces;
+	}
+
+	public function setCControlacces($cControlacces) {
+		$this->cControlacces = $cControlacces;
+	}
+
+	public function getCDaoloc() {
+		return $this->cDaoloc;
+	}
+
+	public function setCDaoloc($cDaoloc) {
+		$this->cDaoloc = $cDaoloc;
+	}
+
 }
 ?>
