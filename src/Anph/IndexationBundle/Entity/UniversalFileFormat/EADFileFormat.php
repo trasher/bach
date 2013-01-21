40c5c976ddbb5d99e17f794fa0143c789b5b7936
forest <jocelyn.fores@esial.net>
2013-01-21 16:44:06 +0100

    MAp

--- src/Anph/IndexationBundle/Entity/UniversalFileFormat/EADFileFormat.php
+++ src/Anph/IndexationBundle/Entity/UniversalFileFormat/EADFileFormat.php
@@ -12,29 +12,25 @@ use Doctrine\ORM\Mapping as ORM;
 */
 class EADFileFormat extends UniversalFileFormat
 {
-	protected $accessRestrict;
-	protected $abbr;
-	protected $abstract;
-	protected $accruals;
-	protected $acqinfo;
-	protected $address;
-	protected $altformavail;
-	protected $appraisal;
-	protected $archref;
-	protected $arrangement;
-	protected $author;
-	protected $bibliography;
-	protected $bibref;
-	protected $bibseries;
-	protected $bioghist;
-	protected $container;
-	protected $controlaccess;
-	protected $custodhist;
-	protected $date;
-	protected $daodesc;
-	protected $did;
-	protected $dimensions;
-	protected $edition, $expan, $extent, $extref, $ext, $extrefloc, $fileplan, $frontmatter, $imprint, $index, $indexentry, $langmaterial, $language, $legalstatus, $materialspec, $note, $num, $odd, $originals ;
+	protected $archeDescRootUnitid,
+                    $archeDescRootUnittitle,
+                    $archeDescRootUnitdate,
+                    $archeDescRootPhysdesc,
+                    $archeDescRootRepository,
+                    $archeDescRootLangmaterial,
+                   	$archeDescRootOrigination,
+                    $archeDescRootAcqinfo,
+                    $archeDescRootScopecontent,
+                    $archeDescRootAccruals,
+                    $archeDescRootArrangement,
+                    $archeDescRootAccessrestrict,
+                    $archeDescRootLegalstatus,
+                    $archeDescRootUserestrict,
+                    $archeDescRootOriginalsloc,
+                    $archeDescRootRelatedmaterial,
+                    $archeDescRootOdd,
+                    $archeDescRootProcessinfo,
+                    $archeDescRootControlaccess;
 	
 	
 	
