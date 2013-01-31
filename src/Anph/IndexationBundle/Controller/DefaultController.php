3732e954b3dd0b07c4fbeb4329ee0bff6f8e7e8d
rems <rems@remsPC>
2013-01-31 22:09:18 +0100

    Upload fichier one by one

--- src/Anph/IndexationBundle/Controller/DefaultController.php
+++ src/Anph/IndexationBundle/Controller/DefaultController.php
@@ -15,83 +15,13 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-    	/*
-    	$manager = $this->container->get('anph.indexation.file_driver_manager');
-    	$factory = $this->container->get('anph.indexation.data_bag_factory'); // Fourni le bon DataBag pour le fichier à indexer
-    
-    	//$fileInfo = new \SplFileInfo(__DIR__.'/FRAD027_PC.xml');
-    	$fileInfo = new \SplFileInfo(__DIR__.'/../Resources/benchmark/data/EAD/6_FRAD027_404142R_220mo.xml');
-    	//$fileInfo = new \SplFileInfo(__DIR__.'/fsor2709.c01');
-    	
-    	try{
-    		$universalFileFormats = $manager->convert($factory->encapsulate($fileInfo),'ead');
-		//	var_dump($universalFileFormats);
-    	}catch(BadInputFileFormatException $e){
-    		echo $e->getMessage();
-    	}
-    	echo memory_get_usage() . "\n"; // 36640
-    	   /**/
-    	$em = $this->getDoctrine()->getEntityManager();
-    	
-    	$file = __DIR__.'/FRAD027_404142R.xml';
-    	$format = 'unimarc';
-    	
-    	
-    	$task = new ArchFileIntegrationTask($file, $format);
-    	$em->persist($task);
-    	//$em->flush();
-    	
-    	$repository = $em
-    				->getRepository('AnphIndexationBundle:ArchFileIntegrationTask');
-    	
-    	$entities = $repository
-    				->createQueryBuilder('t')
-    				->orderBy('t.taskId', 'DESC')
-    				->getQuery()
-    				->getResult();
-    	$tasks = array();
-    
-    	foreach ($entities as $entity) {
-    		$spl = new \SplFileInfo($entity->getFilename());
-    		$tasks[] = array(	'filename'	=>	$spl->getBasename(),
-    							'format'	=>	$entity->getFormat(),
-    							'size'		=>	$spl->getSize());
-   
-    		switch ((int)$entity->getStatus()) {
-    			default:
-    			case 0:
-    				$status = "";
-    				break;
-    				
-    			case 1:
-    				$status = "success";
-    				break;
-    				
-    			case 2:
-    			case 3:
-    				$status = "error";
-    				break;
-    		}
-    		
-    		$tasks[count($tasks)-1]['status'] = $status;
-    	}
-    	
-    	
-		return $this->render('AnphIndexationBundle:Indexation:index.html.twig',array('tasks'=>$tasks));
-    }
-    
-	/**
-	 * @Route("/upload", name="upload")
-	 * @Template()
-	 */
-	public function uploadAction()
-	{
-	    $document = new Document();
+    	$document = new Document();
 	    $form = $this->createFormBuilder($document)
 	       // ->add('name')
 	        ->add('file')
 	        ->getForm()
 	    ;
+	    
 	
 	    if ($this->getRequest()->isMethod('POST')) {
 	        $form->bind($this->getRequest());
@@ -149,11 +79,56 @@ class DefaultController extends Controller
 	    	}
 	    	
 	    	
-			return $this->render('AnphIndexationBundle:Indexation:index.html.twig',array('tasks'=>$tasks));
+		return $this->render('AnphIndexationBundle:Indexation:index.html.twig',array('tasks'=>$tasks,'form' => $form->createView()));
+	    	
 		       }
 	    }
+	    else{
+	    	$name = null;
+	    	$format = null;
+	    	$em2 = $this->getDoctrine()->getManager();
+	    	 
+	    	$format = $document->getExtension();
+	    	
+	    	$task = new ArchFileIntegrationTask($name, $format);
+	    	$em2->persist($task);
+	    	
+	    	$repository = $em2
+	    	->getRepository('AnphIndexationBundle:ArchFileIntegrationTask');
+	    	 
+	    	$entities = $repository
+	    	->createQueryBuilder('t')
+	    	->orderBy('t.taskId', 'DESC')
+	    	->getQuery()
+	    	->getResult();
+	    	$tasks = array();
+	    	 
+	    	foreach ($entities as $entity) {
+	    		$spl = new \SplFileInfo($entity->getFilename());
+	    		$tasks[] = array(	'filename'	=>	$spl->getBasename(),
+	    				'format'	=>	$entity->getFormat(),
+	    				'size'		=>	$spl->getSize());
+	    	
+	    		switch ((int)$entity->getStatus()) {
+	    			default:
+	    			case 0:
+	    				$status = "";
+	    				break;
+	    				 
+	    			case 1:
+	    				$status = "success";
+	    				break;
+	    				 
+	    			case 2:
+	    			case 3:
+	    				$status = "error";
+	    				break;
+	    		}
+	    		 
+	    		$tasks[count($tasks)-1]['status'] = $status;
+	    	}
 	
-	    return array('form' => $form->createView());
-	   // return $this->render('AnphIndexationBundle:Indexation:index.html.twig',array('form' => $form->createView()));
-	}
+		return $this->render('AnphIndexationBundle:Indexation:index.html.twig',array('tasks'=>$tasks,'form' => $form->createView()));
+    	}
+    }
 }
