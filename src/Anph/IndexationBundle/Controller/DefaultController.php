622c23a60fe5964bc6f2e2098ff7aa1a8c63c19f
rems <rems@remsPC>
2013-01-31 23:00:40 +0100

    Uploader modifier N'oubliez pas de mettre à jour la BDD Allez dans /var/www/bach et exécutez php app/console doctrine:schema:update --force

--- src/Anph/IndexationBundle/Controller/DefaultController.php
+++ src/Anph/IndexationBundle/Controller/DefaultController.php
@@ -18,7 +18,10 @@ class DefaultController extends Controller
     	$document = new Document();
 	    $form = $this->createFormBuilder($document)
 	       // ->add('name')
-	        ->add('file')
+	        ->add('file')/*,'file',array(
+            "label" => "Fichiers",
+            "attr" => array(
+                "multiple" => "multiple")))*/
 	        ->getForm()
 	    ;
 	    
@@ -27,56 +30,57 @@ class DefaultController extends Controller
 	        $form->bind($this->getRequest());
 	        if ($form->isValid()) {
 	        	
-	        	$path = $document->getPath();
-	        	$name = $document->getName();
-	        	
-	            $em = $this->getDoctrine()->getManager();
-
+		        	$path = $document->getPath();
+		        	$name = $document->getName();
+		        	
+		            $em = $this->getDoctrine()->getManager();
 	
-	            $em->persist($document);
-	            $em->flush();
-
-	            $em2 = $this->getDoctrine()->getManager();
-	            
-	            $format = $document->getExtension();
-	             
-	            $task = new ArchFileIntegrationTask($name, $format);
-	            $em2->persist($task);
+		
+		            $em->persist($document);
+		            $em->flush();
 	
-	           $repository = $em2
-    				->getRepository('AnphIndexationBundle:ArchFileIntegrationTask');
-    	
-	    	$entities = $repository
-	    				->createQueryBuilder('t')
-	    				->orderBy('t.taskId', 'DESC')
-	    				->getQuery()
-	    				->getResult();
-	    	$tasks = array();
-	    
-	    	foreach ($entities as $entity) {
-	    		$spl = new \SplFileInfo($entity->getFilename());
-	    		$tasks[] = array(	'filename'	=>	$spl->getBasename(),
-	    							'format'	=>	$entity->getFormat(),
-	    							'size'		=>	$spl->getSize());
-	   
-	    		switch ((int)$entity->getStatus()) {
-	    			default:
-	    			case 0:
-	    				$status = "";
-	    				break;
-	    				
-	    			case 1:
-	    				$status = "success";
-	    				break;
-	    				
-	    			case 2:
-	    			case 3:
-	    				$status = "error";
-	    				break;
-	    		}
-	    		
-	    		$tasks[count($tasks)-1]['status'] = $status;
-	    	}
+		            $em2 = $this->getDoctrine()->getManager();
+		            
+		            $format = $document->getExtension();
+		             
+		            $task = new ArchFileIntegrationTask($name, $format);
+		            $em2->persist($task);
+		            
+		
+		           $repository = $em2
+	    				->getRepository('AnphIndexationBundle:ArchFileIntegrationTask');
+	    	
+		    	$entities = $repository
+		    				->createQueryBuilder('t')
+		    				->orderBy('t.taskId', 'DESC')
+		    				->getQuery()
+		    				->getResult();
+		    	$tasks = array();
+		    
+		    	foreach ($entities as $entity) {
+		    		$spl = new \SplFileInfo($entity->getFilename());
+		    		$tasks[] = array(	'filename'	=>	$spl->getBasename(),
+		    							'format'	=>	$entity->getFormat(),
+		    							'size'		=>	$spl->getSize());
+		   
+		    		switch ((int)$entity->getStatus()) {
+		    			default:
+		    			case 0:
+		    				$status = "";
+		    				break;
+		    				
+		    			case 1:
+		    				$status = "success";
+		    				break;
+		    				
+		    			case 2:
+		    			case 3:
+		    				$status = "error";
+		    				break;
+		    		}
+		    		
+		    		$tasks[count($tasks)-1]['status'] = $status;
+		    	}
 	    	
 	    	
 		return $this->render('AnphIndexationBundle:Indexation:index.html.twig',array('tasks'=>$tasks,'form' => $form->createView()));
@@ -88,7 +92,6 @@ class DefaultController extends Controller
 	    	$format = null;
 	    	$em2 = $this->getDoctrine()->getManager();
 	    	 
-	    	$format = $document->getExtension();
 	    	
 	    	$task = new ArchFileIntegrationTask($name, $format);
 	    	$em2->persist($task);
