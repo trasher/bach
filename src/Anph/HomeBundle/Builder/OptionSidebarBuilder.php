a37da0c2c2d202b52f24d7bbd84c1f3543eebcda
Robin Bressan <robin.bressan@pilooho.com>
2013-03-13 13:52:52 +0100

    Gestion sidebar HomeBundle

---
+++ src/Anph/HomeBundle/Builder/OptionSidebarBuilder.php
@@ -0,0 +1,65 @@
+<?php 
+
+namespace Anph\HomeBundle\Builder;
+
+use Anph\HomeBundle\Entity\Sidebar\OptionSidebar;
+
+class OptionSidebarBuilder
+{
+	private $sidebar;
+	
+	public function __construct(OptionSidebar $sidebar){
+		$this->sidebar = $sidebar;
+	}
+	
+	public function compileToArray(){
+		$output = array();
+		
+		$items = $this->sidebar->getItems();
+		
+		$linkValues = array();
+		
+		foreach($items as $item){
+			$output[$item->getName()] = array();
+			
+			foreach($item->getChoices() as $choice){
+				
+				if($choice->isSelected()){
+					$linkValues[$item->getKey()] = $choice->getValue();
+				}
+				
+				$output[$item->getName()][] = array(	"alias"		=>	$choice->getAlias(),
+														"key"		=>	$item->getKey(),
+														"value"		=>	$choice->getValue(),
+														"selected"	=>	$choice->isSelected());
+			}
+		}
+		
+		
+		$urlParams = "";
+		
+		foreach($output as $itemName => $item){
+			foreach($item as $key=>$choice){				
+				$url = "";
+				foreach($linkValues as $linkKey=>$linkValue){
+					$url .= $linkKey."=";
+					
+					if($linkKey == $choice["key"]){
+						$url .= $choice["value"];
+					}else{
+						$url .= $linkValue;
+					}
+					
+					$url .= "&";
+				}
+				$url = substr($url,0,strlen($url)-1);
+				
+				$output[$itemName][$key]["url"] = $url;				
+			}
+		}
+		
+		return $output;
+	}
+}
+
+?>
---
+++ src/Anph/HomeBundle/Entity/Sidebar/OptionSidebarItem.php
@@ -0,0 +1,40 @@
+<?php 
+
+namespace Anph\HomeBundle\Entity\Sidebar;
+
+class OptionSidebarItem
+{
+	private $name;
+	
+	private $choices;
+	
+	private $key;
+	
+	public function __construct($name, $key, $default){
+		$this->name = $name;
+		$this->key = $key;
+		$this->default = $default;
+	}
+	
+	public function getName(){
+		return $this->name;
+	}
+	
+	public function getKey(){
+		return $this->key;
+	}
+	
+	public function getDefault(){
+		return $this->default;
+	}
+	
+	public function appendChoice(OptionSidebarItemChoice $choice){
+		$this->choices[] = $choice;
+		
+		return $this;
+	}
+	
+	public function getChoices(){
+		return $this->choices;
+	}
+}
