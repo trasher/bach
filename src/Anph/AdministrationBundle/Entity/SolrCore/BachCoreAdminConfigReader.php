4fdef3895cf444f8b275bab227bdce954161ed02
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-03-18 17:37:08 +0100

    Replace DOM functions with less verbose simplexml ones

--- src/Anph/AdministrationBundle/Entity/SolrCore/BachCoreAdminConfigReader.php
+++ src/Anph/AdministrationBundle/Entity/SolrCore/BachCoreAdminConfigReader.php
@@ -10,92 +10,100 @@ use DOMDocument;
 class BachCoreAdminConfigReader
 {
     const CONFIG_FILE_NAME = "BachSolrCoreConfig.xml";
-    
+
     private $doc;
-    
+
     /**
      * Constructor. Reads the config XML file.
      */
     public function __construct() {
-        $this->doc = new DOMDocument();
-        $this->doc->load(__DIR__.'/../../Resources/config/' . self::CONFIG_FILE_NAME);
+        $filepath = __DIR__.'/../../Resources/config/' . self::CONFIG_FILE_NAME;
+        libxml_use_internal_errors(true);
+        $this->doc = simplexml_load_file($filepath);
+
+        if ( $this->doc === false ) {
+            $msg = __METHOD__ . ' | An error occured loading XML file ' .
+                $filepath . ":\n";
+            foreach ( libxml_get_errors() as $error ) {
+                $msg .= "\t" . $error->message . "\n";
+            }
+            throw new \RuntimeException($msg);
+        }
     }
-    
+
     /**
      * Get system path to cores' directory.
      * @return string
      */
     public function getCoresPath()
     {
-        return $this->doc->getElementsByTagName('solrCoresPath')->item(0)->nodeValue;
+        return $this->doc->solrCoresPath;
     }
-    
+
     /**
      * Get Solr URL for sending core administration queries.
      * @return string
      */
     public function getCoresURL()
     {
-        return $this->doc->getElementsByTagName('solrCoresURL')->item(0)->nodeValue;
+        return $this->doc->solrCoresURL;
     }
-    
+
     /**
      * Get core template. This template is used for create a new core.
      * @return string
      */
     public function getCoreTemplatePath()
     {
-        return $this->doc->getElementsByTagName('solrCoreTemplatePath')->item(0)->nodeValue;
+        return $this->doc->solrCoreTemplatePath;
     }
-    
+
     /**
      * Get name of core data directory (indexes...).
      * @return string
      */
     public function getCoreDataDir()
     {
-        return $this->doc->getElementsByTagName('solrCoreDataDirectoryName')->item(0)->nodeValue;
+        return $this->odc->solrCoreDataDirectoryName;
     }
-    
+
     /**
      * Get name of core config directory (schema.xml, solrconfig.xml...).
      * @return string
      */
     public function getCoreConfigDir()
     {
-        return $this->doc->getElementsByTagName('solrCoreConfigDirectoryName')->item(0)->nodeValue;
+        return $this->doc->solrCoreConfigDirectoryName;
     }
-    
+
     /**
      * Get name of configuration file (usually solrconfig.xml)
      * @return string
      */
     public function getConfigFileName()
     {
-        return $this->doc->getElementsByTagName('solrConfigFileName')->item(0)->nodeValue;
+        return $this->doc->solrConfigFileName;
     }
-    
+
     /**
      * Get name of schema file (usually schema.xml)
      * @return string
      */
     public function getSchemaFileName()
     {
-        return $this->doc->getElementsByTagName('solrSchemaFileName')->item(0)->nodeValue;
+        return $this->doc->solrSchemaFileName;
     }
-    
+
     public function getDataConfigFileName()
     {
-        return $this->doc->getElementsByTagName('solrDataConfigFileName')->item(0)->nodeValue;
+        return $this->doc->solrDataConfigFileName;
     }
-    
+
     public function getDatabaseParameters()
     {
         $data = array();
-        $elt = $this->doc->getElementsByTagName('databaseConfig')->item(0);
-        $eltChilds = $elt->childNodes;
-        foreach ($eltChilds as $e) {
-            $data[$e->nodeName] = $e->nodeValue;
+        foreach ( $this->doc->databaseConfig as $e  ) {
+            $data[$e->getName()] = (string)$e;
         }
         return $data;
     }
