9edcc78274a4a4515044f1bd58fedd75be04613d
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-07-02 12:32:40 +0200

    PHPDoc, coding standards

--- src/Anph/IndexationBundle/Command/ArchFileIntegrationCommand.php
+++ src/Anph/IndexationBundle/Command/ArchFileIntegrationCommand.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Archival file integration in database from command line
+ *
+ * PHP version 5
+ *
+ * @category Indexation
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
+
 namespace Anph\IndexationBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
@@ -10,25 +22,49 @@ use Anph\IndexationBundle\Generator\FileDriverGenerator;
 use Symfony\Component\Finder\SplFileInfo;
 use Symfony\Component\Finder\Finder;
 
+/**
+ * Archival file integration in database from command line
+ *
+ * PHP version 5
+ *
+ * @category Indexation
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
 class ArchFileIntegrationCommand extends ContainerAwareCommand
 {
-	protected function configure()
+    /**
+     * Configures command
+     *
+     * @return void
+     */
+    protected function configure()
     {
         $this
             ->setName('anph:process:archfileintegration')
             ->setDescription('Integrate archivists file')
-        	->setHelp(<<<EOF
+            ->setHelp(
+                <<<EOF
 The <info>%command.name%</info> command convert a archivist file in format for SOLR engine
 EOF
-        );
+            );
     }
 
+    /**
+     * Executes the command
+     *
+     * @param InputInterface  $input  Stdin
+     * @param OutputInterface $output Stdout
+     *
+     * @return void
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-    	$integrationService = $this->getContainer()->get('anph.indexation.process.arch_file_integration');
-    	
-    	$return = $integrationService->integrate();
-    	
-    	$output->writeln($return);
+        $integrationService = $this->getContainer()
+            ->get('anph.indexation.process.arch_file_integration');
+        $return = $integrationService->integrate();
+        $output->writeln($return);
     }
 }
--- src/Anph/IndexationBundle/Service/ArchFileIntegration.php
+++ src/Anph/IndexationBundle/Service/ArchFileIntegration.php
@@ -1,74 +1,106 @@
 <?php
+/**
+ * Archival file integration in database
+ *
+ * PHP version 5
+ *
+ * @category Indexation
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
 
 namespace Anph\IndexationBundle\Service;
 
 use Anph\AdministrationBundle\Entity\SolrCore\SolrCoreAdmin;
-
 use Anph\IndexationBundle\Exception\BadInputFileFormatException;
 use Anph\IndexationBundle\Exception\UnknownDriverParserException;
 use Doctrine\ORM\EntityManager;
 
+/**
+ * Archival file integration in database
+ *
+ * PHP version 5
+ *
+ * @category Indexation
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
 class ArchFileIntegration
 {
-	private $manager;
-	private $factory;
-	private $entityManager;
-	
-	public function __construct(FileDriverManager $manager, DataBagFactory $factory, EntityManager $entityManager)
-	{
-		$this->manager = $manager;
-		$this->factory = $factory;
-		$this->entityManager = $entityManager;
-	}
-	
-	public function integrate() 
-	{
-		$repository = $this->entityManager
-		->getRepository('AnphIndexationBundle:ArchFileIntegrationTask');
-		
-		$tasks = $repository->findByStatus(0);
-		
-		foreach ($tasks as $task) {
-			$spl = new \SplFileInfo($task->getPath());
-			$format = $task->getFormat();
-			$preprocessor = $task->getPreprocessor();
-			
-			try{
-				$universalFileFormats = $this->manager->convert($this->factory->encapsulate($spl),$format, $preprocessor);
-				
-				foreach ($universalFileFormats as $universalFileFormat) {
-					$this->entityManager->persist($universalFileFormat);
-				}
-				
-				$task->setStatus(1);
-				//$this->entityManager->remove($task);
-				$this->entityManager->persist($task);
-				$this->entityManager->flush();
-				
-				
+    private $_manager;
+    private $_factory;
+    private $_entityManager;
 
-				$sca = new SolrCoreAdmin();
-				$coreNames = $sca->getStatus()->getCoreNames();
-				foreach ($coreNames as $core){
-					$sca->fullImport($core);
-				}
-				
-				
-		
-			}catch(BadInputFileFormatException $e){
-				$task->setStatus(2);
-				$this->entityManager->persist($task);
-				$this->entityManager->flush();
-			}catch(UnknownDriverParserException $e){
-				$task->setStatus(3);
-				$this->entityManager->persist($task);
-				$this->entityManager->flush();
-			}catch(\DomainException $e){
-				$task->setStatus(4);
-				$this->entityManager->persist($task);
-				$this->entityManager->flush();
-			}	
-		}
-		
-	}
+    /**
+     * Instanciate Service
+     *
+     * @param FileDriverManager $manager       The file driver manager
+     * @param DataBagFactory    $factory       The databag factory instance
+     * @param EntityManager     $entityManager The entity manager
+     */
+    public function __construct(FileDriverManager $manager,
+        DataBagFactory $factory, EntityManager $entityManager
+    ) {
+        $this->_manager = $manager;
+        $this->_factory = $factory;
+        $this->_entityManager = $entityManager;
+    }
+
+    /**
+     * Intagrate files in queue into the database
+     *
+     * @return void
+     */
+    public function integrate()
+    {
+        $repository = $this->_entityManager
+            ->getRepository('AnphIndexationBundle:ArchFileIntegrationTask');
+
+        $tasks = $repository->findByStatus(0);
+
+        foreach ($tasks as $task) {
+            $spl = new \SplFileInfo($task->getPath());
+            $format = $task->getFormat();
+            $preprocessor = $task->getPreprocessor();
+
+            try{
+                $universalFileFormats = $this->_manager->convert(
+                    $this->_factory->encapsulate($spl),
+                    $format,
+                    $preprocessor
+                );
+
+                foreach ($universalFileFormats as $universalFileFormat) {
+                    $this->_entityManager->persist($universalFileFormat);
+                }
+
+                $task->setStatus(1);
+                //$this->_entityManager->remove($task);
+                $this->_entityManager->persist($task);
+                $this->_entityManager->flush();
+
+                $sca = new SolrCoreAdmin();
+                $coreNames = $sca->getStatus()->getCoreNames();
+                foreach ( $coreNames as $core ) {
+                    $sca->fullImport($core);
+                }
+            }catch(BadInputFileFormatException $e){
+                $task->setStatus(2);
+                $this->_entityManager->persist($task);
+                $this->_entityManager->flush();
+            }catch(UnknownDriverParserException $e){
+                $task->setStatus(3);
+                $this->_entityManager->persist($task);
+                $this->_entityManager->flush();
+            }catch(\DomainException $e){
+                $task->setStatus(4);
+                $this->_entityManager->persist($task);
+                $this->_entityManager->flush();
+            }
+        }
+    }
 }
