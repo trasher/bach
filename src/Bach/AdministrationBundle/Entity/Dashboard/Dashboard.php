0037617f58153ca20c730629215d429522600bab
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-07-29 16:36:03 +0200

    Indent, remove not needeed empty constructor, coding standards

--- src/Bach/AdministrationBundle/Entity/Dashboard/Dashboard.php
+++ src/Bach/AdministrationBundle/Entity/Dashboard/Dashboard.php
@@ -1,59 +1,90 @@
 <?php
+/**
+ * Bach dashboard utilities
+ *
+ * PHP version 5
+ *
+ * @category Administration
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
+
 namespace Bach\AdministrationBundle\Entity\Dashboard;
 
 use DOMDocument;
 
 /**
- * Reads the Solr core config xml file for retreive information like cores' path, solr URL etc.  
+ * Bach dashboard utilities
  *
+ * PHP version 5
+ *
+ * @category Administration
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
  */
 class Dashboard
 {
-    
-
-    
     /**
-     * Constructor. Reads the config XML file.
-     */
-    
-    public function __construct() {
-        
-    }
-    
-    /**
-     * Get system path to cores' directory.
+     * Get system total virtual memory
+     *
      * @return string
      */
     public function getSystemTotalVirtualMemory()
     {
-        $a = exec('awk \'/MemTotal/ {printf( "%.2f\n", $2 / 1024 )}\' /proc/meminfo',$status,$a);
-    	return $a;
-    
+        $a = exec(
+            'awk \'/MemTotal/ {printf( "%.2f\n", $2 / 1024 )}\' /proc/meminfo',
+            $status,
+            $a
+        );
+        return $a;
     }
-    
+
+    /**
+     * Get system free virtual memory
+     *
+     * @return string
+     */
     public function getSystemFreeVirtualMemory()
     {
-    	$a =exec('awk \'/MemFree/ {printf( "%.2f\n", $2 / 1024 )}\' /proc/meminfo',$status,$a);
+        $a =exec(
+            'awk \'/MemFree/ {printf( "%.2f\n", $2 / 1024 )}\' /proc/meminfo',
+            $status,
+            $a
+        );
         return $a;
     }
-    
-    
+
+    /**
+     * Get system total swap
+     *
+     * @return string
+     */
     public function getSystemTotalSwapMemory()
     {
-    	
-    	 $a= exec('awk \'/SwapTotal/ {printf( "%.2f\n", $2 / 1024 )}\' /proc/meminfo',$status,$a);
+        $a= exec(
+            'awk \'/SwapTotal/ {printf( "%.2f\n", $2 / 1024 )}\' /proc/meminfo',
+            $status,
+            $a
+        );
         return $a;
     }
-    
+
+    /**
+     * Get system free swap
+     *
+     * @return string
+     */
     public function getSystemFreeSwapMemory()
     {
-       $a= exec('awk \'/SwapFree/ {printf( "%.2f\n", $2 / 1024 )}\' /proc/meminfo',$status,$a);
+        $a= exec(
+            'awk \'/SwapFree/ {printf( "%.2f\n", $2 / 1024 )}\' /proc/meminfo',
+            $status,
+            $a
+        );
         return $a;
     }
-    
-    
-    
-    
-    
- 
 }
