557a4bf9cac19e8ba31ed6bfa82fcafdf09172e7
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-07-29 12:40:12 +0200

    Replace tabs by spaces

--- src/Bach/AdministrationBundle/Controller/DefaultController.php
+++ src/Bach/AdministrationBundle/Controller/DefaultController.php
@@ -21,33 +21,33 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
-	public function indexAction()
-	{
-		return $this->render('AdministrationBundle:Default:index.html.twig');
-	}
-	
-	public function coreadminAction()
-	{
-		return $this->render('AdministrationBundle:Default:coreadmin.html.twig');
-	}
-	
-	public function performanceAction()
-	{
-		return $this->render('AdministrationBundle:Default:performance.html.twig');
-	}
-	
-	public function dashboardAction()
-	{
-	    $coreName = $this->getRequest()->request->get('selectedCore');
-	    if (!isset($coreName)) {
-	        $coreName = 'none';
-	    }
-	    $sca = new SolrCoreAdmin();
-	    $coreNames = $sca->getStatus()->getCoreNames();
-	    $coresInfo = array();
-	    foreach ($coreNames as $cn) {
-	        $coresInfo[$cn] = new CoreStatus($cn);
-	    }
+    public function indexAction()
+    {
+        return $this->render('AdministrationBundle:Default:index.html.twig');
+    }
+
+    public function coreadminAction()
+    {
+        return $this->render('AdministrationBundle:Default:coreadmin.html.twig');
+    }
+
+    public function performanceAction()
+    {
+        return $this->render('AdministrationBundle:Default:performance.html.twig');
+    }
+
+    public function dashboardAction()
+    {
+        $coreName = $this->getRequest()->request->get('selectedCore');
+        if (!isset($coreName)) {
+            $coreName = 'none';
+        }
+        $sca = new SolrCoreAdmin();
+        $coreNames = $sca->getStatus()->getCoreNames();
+        $coresInfo = array();
+        foreach ($coreNames as $cn) {
+            $coresInfo[$cn] = new CoreStatus($cn);
+        }
         $session = $this->getRequest()->getSession();
         $session->set('coreNames', $coreNames);
         $session->set('coreName', $coreName);
@@ -56,22 +56,22 @@ class DefaultController extends Controller
         } else {
             $session->set('xmlP', new XMLProcess($coreName));
         }
-        
+
         $db = new Dashboard();
-        
+
         $SystemFreeVirtualMemory=$db->getSystemFreeVirtualMemory();
         $SystemUsedVirtualMemory = $db->getSystemTotalVirtualMemory()-$SystemFreeVirtualMemory;
         $SystemFreeSwapMemory=$db->getSystemFreeSwapMemory();
         $SystemUsedSwapMemory=$db->getSystemTotalSwapMemory()-$SystemFreeSwapMemory;
-        
+
         return $this->render('AdministrationBundle:Default:dashboard.html.twig', array(
                 'coreName'  => $coreName,
                 'coreNames' => $coreNames,
-        		'SystemUsedVirtualMemory' => $SystemUsedVirtualMemory,
-        		'SystemFreeVirtualMemory' => $SystemFreeVirtualMemory,
-        		'SystemUsedSwapMemory' => $SystemUsedSwapMemory,
-        		'SystemFreeSwapMemory' => $SystemFreeSwapMemory,
+                'SystemUsedVirtualMemory' => $SystemUsedVirtualMemory,
+                'SystemFreeVirtualMemory' => $SystemFreeVirtualMemory,
+                'SystemUsedSwapMemory' => $SystemUsedSwapMemory,
+                'SystemFreeSwapMemory' => $SystemFreeSwapMemory,
                 'coresInfo' => $coresInfo
                 ));
-	}
+    }
 }
