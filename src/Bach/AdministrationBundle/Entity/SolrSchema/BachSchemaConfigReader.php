4a9588f10f3c704b1041008e248e4c223c84acbe
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-07-29 16:49:16 +0200

    PHPDoc, coding standards

--- src/Bach/AdministrationBundle/Entity/SolrSchema/BachSchemaConfigReader.php
+++ src/Bach/AdministrationBundle/Entity/SolrSchema/BachSchemaConfigReader.php
@@ -1,12 +1,31 @@
 <?php
+/**
+ * Bach Solr schema configuration reader
+ *
+ * PHP version 5
+ *
+ * @category Administration
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
+
 namespace Bach\AdministrationBundle\Entity\SolrSchema;
 
 use DOMDocument;
 use DOMNodeList;
 
 /**
- * This class allow developers to read bachconfig.xml file, which describes some schema.xml tags, attributes etc.
+ * Bach Solr schema configuration reader
  *
+ * PHP version 5
+ *
+ * @category Administration
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
  */
 class BachSchemaConfigReader
 {
@@ -18,116 +37,140 @@ class BachSchemaConfigReader
     const COPY_FIELD_TAG = 'copyField';
     const UNIQUE_KEY_TAG = 'uniqueKey';
     const ANALYZER_TAG = 'analyzer';
-    
-    private $doc;
-    private $lang;
-    private $defaultLang;
-    
+
+    private $_doc;
+    private $_lang;
+    private $_defaultLang;
+
     /**
      * Constructor. Reads xml config file.
      */
     public function __construct()
     {
-        $this->doc = new DOMDocument();
-        $this->doc->load(__DIR__.'/../../Resources/config/' . self::CONFIG_FILE_NAME);
-        $this->lang = $this->doc->getElementsByTagName('lang')->item(0)->nodeValue;
-        $this->defaultLang = $this->doc->getElementsByTagName('defaultLang')->item(0)->nodeValue;
+        $this->_doc = new DOMDocument();
+        $this->_doc->load(
+            __DIR__ . '/../../Resources/config/' . self::CONFIG_FILE_NAME
+        );
+        $this->_lang = $this->_doc->getElementsByTagName('lang')
+            ->item(0)->nodeValue;
+        $this->_defaultLang = $this->_doc->getElementsByTagName('defaultLang')
+            ->item(0)->nodeValue;
     }
-    
+
     /**
      * Get a tag by its name.
+     *
      * @param string $name must be one of the class's constants.
-     * @return NULL|\Bach\AdministrationBundle\Entity\SolrSchema\BachTag
+     *
+     * @return NULL|BachTag
      */
     public function getTagByName($name)
     {
-        $nodeList = $this->doc->getElementsByTagName($tag);
+        $nodeList = $this->_doc->getElementsByTagName($tag);
         if ($nodeList->length == 0) {
             return null;
         } else {
-            return new BachTag($nodeList->item(0), $this->lang, $this->defaultLang);
+            return new BachTag(
+                $nodeList->item(0),
+                $this->_lang,
+                $this->_defaultLang
+            );
         }
     }
-    
+
     /**
      * Get all attributes of the specified tag.
+     *
      * @param string $tag must be one of the class's constants.
-     * @return multitype:\Bach\AdministrationBundle\Entity\SolrSchema\BachAttribute
+     *
+     * @return multitype:BachAttribute
      */
     public function getAttributesByTag($tag)
     {
-        $nodeList = $this->doc->getElementsByTagName($tag)->item(0)->getElementsByTagName("attribute");
-        return $this->retreiveAttributes($nodeList);
+        $nodeList = $this->_doc->getElementsByTagName($tag)
+            ->item(0)->getElementsByTagName('attribute');
+        return $this->_retrieveAttributes($nodeList);
     }
-    
+
     /**
      * Get attribute of the specified tag.
-     * @param string $tag must be one of the class's constants.
+     *
+     * @param string $tag  must be one of the class's constants.
      * @param string $name attribute's name.
-     * @return Ambigous <NULL, \Bach\AdministrationBundle\Entity\SolrSchema\BachAttribute>
+     *
+     * @return BachAttribute
      */
     public function getAttributeByTag($tag, $name)
     {
-        $nodeList = $this->doc->getElementsByTagName($tag)->item(0)->getElementsByTagName("attribute");
-        return $this->retreiveAttribute($nodeList, $name);
+        $nodeList = $this->_doc->getElementsByTagName($tag)
+            ->item(0)->getElementsByTagName("attribute");
+        return $this->_retrieveAttribute($nodeList, $name);
     }
-    
+
     /**
      * Get some extra attributes of the specific class.
+     *
      * @param string $className class name
-     * @return multitype:\Bach\AdministrationBundle\Entity\SolrSchema\BachAttribute |NULL
+     *
+     * @return BachAttribute|NULL
      */
     public function getFieldTypeExtraAttributes($className)
     {
-        $nodeList = $this->doc->getElementsByTagName("fieldTypeExtraAttributes");
+        $nodeList = $this->_doc->getElementsByTagName("fieldTypeExtraAttributes");
         foreach ($nodeList as $e) {
             if ($e.getAttribute("class") == $className) {
                 $nodeList = $e->getElementsByTagName("attribute");
-                return $this->retreiveAttributes($nodeList);
+                return $this->_retrieveAttributes($nodeList);
             }
         }
         return null;
     }
-    
+
     /**
      * Get all classes supporting analyzer.
-     * @return multitype:NULL
+     *
+     * @return array
      */
     public function getAnalyzerSupportingClasses()
     {
         $classes = array();
-        $nodeList = $this->doc->getElementsByTagName('analyzer')->item(0)->getElementsByTagName('value');
+        $nodeList = $this->_doc->getElementsByTagName('analyzer')
+            ->item(0)->getElementsByTagName('value');
         foreach ($nodeList as $e) {
             $classes[] = $e->nodeValue;
         }
         return $classes;
     }
-    
+
     /**
-     * Retreives all attributes from node list;
-     * @param DOMNodeList $nodeList
-     * @return multitype:\Bach\AdministrationBundle\Entity\SolrSchema\BachAttribute
+     * Retrieves all attributes from node list;
+     *
+     * @param DOMNodeList $nodeList Node list
+     *
+     * @return array
      */
-    private function retreiveAttributes(DOMNodeList $nodeList)
+    private function _retrieveAttributes(DOMNodeList $nodeList)
     {
         $attributes = array();
         foreach ($nodeList as $e) {
-            $attributes[] = new BachAttribute($e, $this->lang, $this->defaultLang);
+            $attributes[] = new BachAttribute($e, $this->_lang, $this->_defaultLang);
         }
         return $attributes;
     }
-    
+
     /**
      * Retrieves an attribute from $nodeList by its name.
-     * @param DOMNodeList $nodeList
-     * @param string $name
-     * @return \Bach\AdministrationBundle\Entity\SolrSchema\BachAttribute|NULL
+     *
+     * @param DOMNodeList $nodeList Node list
+     * @param string      $name     Node name to retrieve
+     *
+     * @return BachAttribute|NULL
      */
-    private function retreiveAttribute(DOMNodeList $nodeList, $name)
+    private function _retrieveAttribute(DOMNodeList $nodeList, $name)
     {
         foreach ($nodeList as $e) {
             if ($e->getAttribute("name") == $name) {
-                return new BachAttribute($e, $this->lang, $this->defaultLang);
+                return new BachAttribute($e, $this->_lang, $this->_defaultLang);
             }
         }
         return null;
