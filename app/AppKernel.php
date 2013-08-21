299a1b2ce723e38ee73a0089a8715e960fa2deb6
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-08-21 11:26:35 +0200

    Add cache and logs configuration paths; phpdoc

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,10 +1,36 @@
 <?php
 
+/**
+ * Bach Kernel
+ *
+ * PHP version 5
+ *
+ * @category Core
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
+
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
+/**
+ * Bach Kernel
+ *
+ * @category Core
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
 class AppKernel extends Kernel
 {
+    /**
+     * Returns an array of bundles to registers.
+     *
+     * @return BundleInterface[] An array of bundle instances.
+     */
     public function registerBundles()
     {
         $bundles = array(
@@ -36,8 +62,45 @@ class AppKernel extends Kernel
         return $bundles;
     }
 
+    /**
+     * Loads the container configuration
+     *
+     * @param LoaderInterface $loader A LoaderInterface instance
+     *
+     * @return void
+     */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load(
+            __DIR__ . '/config/config_' . $this->getEnvironment() . '.yml'
+        );
+    }
+
+    /**
+     * Gets the cache directory.
+     *
+     * @return string The cache directory
+     */
+    public function getCacheDir()
+    {
+        if ( defined('BACH_CACHE_DIR') ) {
+            return BACH_CACHE_DIR;
+        } else {
+            return parent::getCacheDir();
+        }
+    }
+
+    /**
+     * Gets the log directory.
+     *
+     * @return string The log directory
+     */
+    public function getLogDir()
+    {
+        if ( defined('BACH_LOG_DIR') ) {
+            return BACH_LOG_DIR;
+        } else {
+            return parent::getLogDir();
+        }
     }
 }
