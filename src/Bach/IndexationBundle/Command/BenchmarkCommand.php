3937e8b8da5ebacdf831ffd55d63a6afaa3f080e
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-08-28 13:56:11 +0200

    Typo in service name, coding standards

--- src/Bach/IndexationBundle/Command/BenchmarkCommand.php
+++ src/Bach/IndexationBundle/Command/BenchmarkCommand.php
@@ -1,4 +1,17 @@
 <?php
+
+/**
+ * Publishing benchmarks
+ *
+ * PHP version 5
+ *
+ * @category Indexation
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
+
 namespace Bach\IndexationBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
@@ -10,62 +23,103 @@ use Bach\IndexationBundle\Generator\FileDriverGenerator;
 use Symfony\Component\Finder\SplFileInfo;
 use Symfony\Component\Finder\Finder;
 
+
+/**
+ * Publishing benchmarks
+ *
+ * PHP version 5
+ *
+ * @category Indexation
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
 class BenchmarkCommand extends ContainerAwareCommand
 {
-	 protected function configure()
+
+    /**
+     * Configures command
+     *
+     * @return void
+     */
+    protected function configure()
     {
         $this
             ->setName('bach:benchmark')
             ->setDescription('Produce benchmarks')
-        	->setHelp(<<<EOF
+            ->setHelp(
+                <<<EOF
 The <info>%command.name%</info> command produce benchmark of bach project
 EOF
-        );
+            );
     }
 
+    /**
+     * Executes the command
+     *
+     * @param InputInterface  $input  Stdin
+     * @param OutputInterface $output Stdout
+     *
+     * @return void
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-    	$manager = $this->getContainer()->get('bach_indexation.file_driver_manager');
-    	$factory = $this->getContainer()->get('bach_indexation.data_bag_factory'); // Fourni le bon DataBag pour le fichier à indexer
-    	$bCount = 1;
-    	$type = 'ead';
-    	$files = $this->generateSPLs($type);
-    	$times = array();
-    	$output->writeln('Benchmarks are running...');
-    	foreach ($files as $file) {
-    		$times[$file->getBasename()] = array();
-    		//$output->writeln('Processing for : '.$file->getBasename());
-    		
-    		for ($i=0;$i<$bCount;$i++) {
-    			
-	    		$t = microtime(true);
-	    		$universalFileFormat = $manager->convert($factory->encapsulate($file),$type);
-	    		$time = microtime(true)-$t;
-	    		
-	    		$times[$file->getBasename()][] = $time;
-    		}
-    	}
-    	$output->writeln("");
-    	$output->writeln("Benchmarks");
-    	$output->writeln("----------");
-    	ksort($times);
-    	foreach ($times as $file=>$fTimes) {
-    		$mean = array_sum($fTimes)/count($fTimes);
-    		$output->writeln($file.' = '.$mean.'s');
-    	}
-    	$output->writeln("");
-    	$output->writeln('Generating benchmarks: <info>OK</info>');
+        $manager = $this->getContainer()->get('bach.indexation.file_driver_manager');
+        //Databag provider
+        $factory = $this->getContainer()->get('bach.indexation.data_bag_factory');
+        $bCount = 1;
+        $type = 'ead';
+        $files = $this->_getBenchFiles($type);
+        $times = array();
+        $output->writeln('Benchmarks are running...');
+        foreach ($files as $file) {
+            $times[$file->getBasename()] = array();
+            $output->writeln('Processing file: '.$file->getBasename());
+
+            for ($i=0;$i<$bCount;$i++) {
+
+                $t = microtime(true);
+                $universalFileFormat = $manager->convert(
+                    $factory->encapsulate($file),
+                    $type
+                );
+                $time = microtime(true)-$t;
+
+                $times[$file->getBasename()][] = $time;
+            }
+        }
+        $output->writeln("");
+        $output->writeln("Benchmarks");
+        $output->writeln("----------");
+        ksort($times);
+        foreach ($times as $file=>$fTimes) {
+            $mean = array_sum($fTimes)/count($fTimes);
+            $output->writeln($file.' = '.$mean.'s');
+        }
+        $output->writeln("");
+        $output->writeln('Generating benchmarks: <info>OK</info>');
     }
-    
-    private function generateSPLs($dir) {
-    	$spl = array();
-    	$finder = new Finder();
-    	$finder->files()->in(__DIR__.'/../Resources/benchmark/data/'.strtoupper($dir))->depth('== 0');
-    	 
-    	foreach ($finder as $file) {
-    		$spl[] = $file;
-    	}
-    	
-    	return $spl;
+
+    /**
+     * Retrieve benchmark files
+     *
+     * @param string $dir Files subdirectory fr type
+     *
+     * @return array
+     */
+    private function _getBenchFiles($dir)
+    {
+        $spl = array();
+        $finder = new Finder();
+        $finder->files()->in(
+            __DIR__ . '/../Resources/benchmark/data/' . strtoupper($dir)
+        )->depth('== 0');
+
+        foreach ($finder as $file) {
+            $spl[] = $file;
+        }
+
+        return $spl;
     }
 }
