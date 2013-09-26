117bb0ab0dac89a22a74e79ef6fa72c80100a548
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-09-26 09:35:05 +0200

    Remove non free google scripts, use jqplot to render memory usage

--- src/Bach/AdministrationBundle/Entity/Dashboard/Dashboard.php
+++ src/Bach/AdministrationBundle/Entity/Dashboard/Dashboard.php
@@ -40,7 +40,7 @@ class Dashboard
             $status,
             $a
         );
-        return $a;
+        return $this->formatBytes($a);
     }
 
     /**
@@ -55,7 +55,8 @@ class Dashboard
             $status,
             $a
         );
-        return $a;
+
+        return $this->formatBytes($a);
     }
 
     /**
@@ -70,7 +71,7 @@ class Dashboard
             $status,
             $a
         );
-        return $a;
+        return $this->formatBytes($a);
     }
 
     /**
@@ -85,6 +86,36 @@ class Dashboard
             $status,
             $a
         );
-        return $a;
+        return $this->formatBytes($a);
+    }
+
+    /**
+     * Format Kb
+     *
+     * @param int $bytes     Bytes amount
+     * @param int $precision Precision
+     *
+     * @return float
+     */
+    function formatBytes($bytes, $precision = 0)
+    {
+        $kilobyte = 1024;
+        $megabyte = $kilobyte * 1024;
+        $gigabyte = $megabyte * 1024;
+        $terabyte = $gigabyte * 1024;
+
+        if (($bytes >= 0) && ($bytes < $kilobyte)) {
+            return $bytes . ' B';
+        } elseif (($bytes >= $kilobyte) && ($bytes < $megabyte)) {
+            return round($bytes / $kilobyte, $precision) . ' KB';
+        } elseif (($bytes >= $megabyte) && ($bytes < $gigabyte)) {
+            return round($bytes / $megabyte, $precision) . ' MB';
+        } elseif (($bytes >= $gigabyte) && ($bytes < $terabyte)) {
+            return round($bytes / $gigabyte, $precision) . ' GB';
+        } elseif ($bytes >= $terabyte) {
+            return round($bytes / $terabyte, $precision) . ' TB';
+        } else {
+            return $bytes . ' B';
+        }
     }
 }
