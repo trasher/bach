abc5e53f4472a8e40dd38b65d62139fb9f3b80bc
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-09-27 12:59:13 +0200

    Coding standards and documentation

--- src/Bach/IndexationBundle/Service/DataBagFactory.php
+++ src/Bach/IndexationBundle/Service/DataBagFactory.php
@@ -1,8 +1,17 @@
 <?php
 
-/*
-* This file is part of the Bach project.
-*/
+/**
+ * Put input file in appropriate DataBag
+ *
+ * PHP version 5
+ *
+ * @category Indexation
+ * @package  Bach
+ * @author   Anaphore PI Team <uknown@unknown.com>
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
 
 namespace Bach\IndexationBundle\Service;
 
@@ -11,22 +20,34 @@ use Bach\IndexationBundle\Entity\Bag\TextDataBag;
 use Bach\IndexationBundle\Entity\Bag\XMLDataBag;
 
 /**
-* DataBagFactory put an input file in an appropriate DataBag
-*/
+ * Put input file in appropriate DataBag
+ *
+ * @category Indexation
+ * @package  Bach
+ * @author   Anaphore PI Team <uknown@unknown.com>
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
 class DataBagFactory
 {
+    /**
+     * Put input file in appropriate DataBag
+     *
+     * @param SplFileInfo $fileInfo FilesController
+     *
+     * @return DataBag
+     */
     public function encapsulate(\SplFileInfo $fileInfo)
     {
-		switch ($fileInfo->getExtension()) {
-			
-			default:
-			case 'txt':
-				return new TextDataBag($fileInfo);
-				break;
-			
-			case 'xml':
-				return new XMLDataBag($fileInfo);
-				break;
-		}
+        switch ($fileInfo->getExtension()) {
+        default:
+        case 'txt':
+            return new TextDataBag($fileInfo);
+            break;
+        case 'xml':
+            return new XMLDataBag($fileInfo);
+            break;
+        }
     }
 }
