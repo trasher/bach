71fe68a1be65b4529cd6cdcdeba3689b3e191846
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-10-09 11:06:52 +0200

    Performance
    Disable debug per default in prod kernel
    Activate Symfony reverse proxy

--- web/app.php
+++ web/app.php
@@ -23,14 +23,16 @@ $loader = include_once __DIR__ . '/../app/bootstrap.php.cache';
  * $loader = new ApcClassLoader('sf2', $loader); $loader->register(true);
  */
 
-$loader = new ApcClassLoader('bach', $loader); $loader->register(true);
+/** FIXME: name *must* be unique if more than one app in sinstalled on the server! */
+$loader = new ApcClassLoader('bach', $loader);
+$loader->register(true);
 
 require_once __DIR__ . '/../app/AppKernel.php';
-// require_once __DIR__.'/../app/AppCache.php';
+require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', true);
+$kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-// $kernel = new AppCache($kernel);
+$kernel = new AppCache($kernel);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
