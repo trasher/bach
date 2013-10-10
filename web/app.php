2882169e34253401acf79e752d42efe68869fb54
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-10-10 16:34:40 +0200

    HTTP Cache cause issues, it would be better to use an external system if needed

--- web/app.php
+++ web/app.php
@@ -28,11 +28,11 @@ $loader = new ApcClassLoader('bach', $loader);
 $loader->register(true);
 
 require_once __DIR__ . '/../app/AppKernel.php';
-require_once __DIR__.'/../app/AppCache.php';
+//require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-$kernel = new AppCache($kernel);
+//$kernel = new AppCache($kernel);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
