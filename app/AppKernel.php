86723ac2aedc5095c51a9d38be2a8c9c257776fe
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-11-25 09:07:56 +0100

    Add console environment

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -75,7 +75,7 @@ class AppKernel extends Kernel
             new FM\ElfinderBundle\FMElfinderBundle()
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test', 'console'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
