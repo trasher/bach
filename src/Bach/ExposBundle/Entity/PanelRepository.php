31ea79db5214099b03fb074af03772285c9d0c7e
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-11-26 11:30:23 +0100

    Add missing expos (empty) repositories

---
+++ src/Bach/ExposBundle/Entity/PanelRepository.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Bach panels repository
+ *
+ * PHP version 5
+ *
+ * @category Expos
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
+
+namespace Bach\ExposBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * PanelRepository
+ *
+ * @category Expos
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
+class PanelRepository extends EntityRepository
+{
+}
