e16f55d3050ef43bdab376b52b06ad14c55c898f
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2013-12-31 14:26:33 +0100

    Skeletons doc and coding standards

--- src/Bach/IndexationBundle/Resources/skeleton/driver/Driver.php
+++ src/Bach/IndexationBundle/Resources/skeleton/driver/Driver.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * {{ format }} file format driver
+ *
+ * PHP version 5
+ *
+ * @category Indexation
+ * @package  Bach
+ * @author   Anaphore PI Team <uknown@unknown.com>
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
 
 namespace {{ namespace }};
 
@@ -7,38 +19,64 @@ use Bach\IndexationBundle\Entity\DataBag;
 use Bach\IndexationBundle\Entity\ObjectTree;
 use Bach\IndexationBundle\Exception\UnknownDriverParserException;
 
+/**
+ * {{ format }} file format driver
+ *
+ * @category Indexation
+ * @package  Bach
+ * @author   Anaphore PI Team <uknown@unknown.com>
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
 class Driver extends FileDriver
-{	
-	/**
-	* {@inheritdoc }
-	*/
-	public function process(DataBag $bag){
-		$parserClass = "Bach\IndexationBundle\Entity\Driver\{{ format_uppercase }}\Parser\\".strtoupper($bag->getType())."\Parser";
-		
-		if (!class_exists($parserClass)) {
-			throw new UnknownDriverParserException(strtoupper($bag->getType()));
-		}
-		
-		$parser = new $parserClass($bag, $this->configuration);
-		$tree = $parser->getTree();
-		return $this->processTree($tree);
-	}
-	
-	/**
-	* {@inheritdoc }
-	*/
-	public function getFileFormatName(){
-		return "{{ format }}";
-	}
-	
-	/**
-	* Process the object tree returned by the parser
-	* @param ObjectTree $tree The parser's tree
-	* @return array Data parsed
-	*/
-	private function processTree(ObjectTree $tree){
-		$results = array();
-		
-		return $results;
-	}
+{
+
+    /**
+     * Perform the parsing of the DataBag
+     *
+     * @param DataBag $bag The data
+     *
+     * @return array
+     */
+    public function process(DataBag $bag)
+    {
+        $parserClass = 'Bach\IndexationBundle\Entity\Driver\{{ format_uppercase }}\Parser\\'.
+            strtoupper($bag->getType()) . '\Parser';
+
+        if (!class_exists($parserClass)) {
+            throw new UnknownDriverParserException(strtoupper($bag->getType()));
+        }
+
+        $parser = new $parserClass($bag, $this->configuration);
+        $tree = $parser->getTree();
+        return $this->_processTree($tree);
+    }
+
+    /**
+     * Get driver format name
+     *
+     * @return string $format The format of the driver
+     *
+     * @return stirng
+     */
+    public function getFileFormatName()
+    {
+        return "{{ format }}";
+    }
+
+    /**
+     * Process the object tree returned by the parser
+     *
+     * @param ObjectTree $tree The parser's tree
+     *
+     * @return array Data parsed
+     */
+    private function _processTree(ObjectTree $tree)
+    {
+        $results = array();
+
+        return $results;
+    }
+
 }
--- src/Bach/IndexationBundle/Resources/skeleton/mapper/DriverMapper.php
+++ src/Bach/IndexationBundle/Resources/skeleton/mapper/DriverMapper.php
@@ -1,12 +1,42 @@
 <?php
+/**
+ * Mapper for {{ mapper }} data
+ *
+ * PHP version 5
+ *
+ * @category Indexation
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
 
 namespace Bach\IndexationBundle\Entity\Mapper;
 
 use Bach\IndexationBundle\DriverMapperInterface;
 
+/**
+ * Mapper for {{ mapper }} data
+ *
+ * PHP version 5
+ *
+ * @category Indexation
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
 class {{ mapper }} implements DriverMapperInterface
 {
-	public function translate($data){
-		return $data;
-	}
+    /**
+     * Translate elements
+     *
+     * @param arrya $data Document data
+     *
+     * @return array
+     */
+    public function translate($data)
+    {
+        return $data;
+    }
 }
