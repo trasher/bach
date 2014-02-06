c81a3ffc574bbfcecbf774b038dc0780a893154b
Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
2014-02-06 09:33:56 +0100

    Add parameter to enable bar, add doc, coding standards

--- src/Bach/HomeBundle/Templating/Helper/SocialBarHelper.php
+++ src/Bach/HomeBundle/Templating/Helper/SocialBarHelper.php
@@ -1,40 +1,111 @@
 <?php
+/**
+ * Social bar helper
+ *
+ * PHP version 5
+ *
+ * @category Templating
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
 
 namespace Bach\HomeBundle\Templating\Helper;
 
 use Symfony\Component\Templating\Helper\Helper;
 use Symfony\Component\Templating\EngineInterface;
 
+/**
+ * Social bar helper
+ *
+ * PHP version 5
+ *
+ * @category Templating
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
 class SocialBarHelper extends Helper
 {
     protected $templating;
 
+    /**
+     * Constructor
+     *
+     * @param EngineInterface $templating Templating instance
+     */
     public function __construct(EngineInterface $templating)
     {
         $this->templating  = $templating;
     }
 
-
+    /**
+     * Get social buttons bar
+     *
+     * @param array $parameters Bar parameters
+     *
+     * @return string
+     */
     public function socialButtons($parameters)
     {
-      return $this->templating->render('BachHomeBundle:helper:socialButtons.html.twig', $parameters);
+        return $this->templating->render(
+            'BachHomeBundle:helper:socialButtons.html.twig',
+            $parameters
+        );
     }
 
+    /**
+     * Get facebook button
+     *
+     * @param array $parameters Button parameters
+     *
+     * @return string
+     */
     public function facebookButton($parameters)
     {
-      return $this->templating->render('BachHomeBundle:helper:facebookButton.html.twig', $parameters);
+        return $this->templating->render(
+            'BachHomeBundle:helper:facebookButton.html.twig',
+            $parameters
+        );
     }
 
+    /**
+     * Get twitter button
+     *
+     * @param array $parameters Button parameters
+     *
+     * @return string
+     */
     public function twitterButton($parameters)
     {
-      return $this->templating->render('BachHomeBundle:helper:twitterButton.html.twig', $parameters);
+        return $this->templating->render(
+            'BachHomeBundle:helper:twitterButton.html.twig',
+            $parameters
+        );
     }
 
+    /**
+     * Get google plus button
+     *
+     * @param array $parameters Button parameters
+     *
+     * @return string
+     */
     public function googlePlusButton($parameters)
     {
-      return $this->templating->render('BachHomeBundle:helper:googlePlusButton.html.twig', $parameters);
+        return $this->templating->render(
+            'BachHomeBundle:helper:googlePlusButton.html.twig',
+            $parameters
+        );
     }
 
+    /**
+     * Helper name
+     *
+     * @return string
+     */
     public function getName()
     {
         return 'socialButtons';
--- src/Bach/HomeBundle/Twig/BachSocialBar.php
+++ src/Bach/HomeBundle/Twig/BachSocialBar.php
@@ -1,74 +1,139 @@
 <?php
+/**
+ * Twig extension to display social buttons
+ *
+ * PHP version 5
+ *
+ * @category Templating
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
 
 namespace Bach\HomeBundle\Twig;
 
-class BachSocialBar extends \Twig_Extension{
+/**
+ * Twig extension to display social buttons
+ *
+ * PHP version 5
+ *
+ * @category Templating
+ * @package  Bach
+ * @author   Johan Cwiklinski <johan.cwiklinski@anaphore.eu>
+ * @license  Unknown http://unknown.com
+ * @link     http://anaphore.eu
+ */
+class BachSocialBar extends \Twig_Extension
+{
 
     protected $container;
 
     /**
      * Constructor.
      *
-     * @param ContainerInterface $container
+     * @param ContainerInterface $container Container
      */
     public function __construct($container)
     {
         $this->container = $container;
     }
 
+    /**
+     * Extension name
+     *
+     * @return string
+     */
     public function getName()
     {
         return 'bach_social_bar';
     }
 
+    /**
+     * Get provided functions
+     *
+     * @return array
+     */
     public function getFunctions()
     {
-      return array(
-        'socialButtons' => new \Twig_Function_Method($this, 'getSocialButtons' ,array('is_safe' => array('html'))),
-        'facebookButton' => new \Twig_Function_Method($this, 'getFacebookLikeButton' ,array('is_safe' => array('html'))),
-        'twitterButton' => new \Twig_Function_Method($this, 'getTwitterButton' ,array('is_safe' => array('html'))),
-        'googlePlusButton' => new \Twig_Function_Method($this, 'getGooglePlusButton' ,array('is_safe' => array('html'))),
-      );
+        return array(
+            'socialButtons' => new \Twig_Function_Method(
+                $this,
+                'getSocialButtons',
+                array('is_safe' => array('html'))
+            ),
+            'facebookButton' => new \Twig_Function_Method(
+                $this,
+                'getFacebookLikeButton',
+                array('is_safe' => array('html'))
+            ),
+            'twitterButton' => new \Twig_Function_Method(
+                $this,
+                'getTwitterButton',
+                array('is_safe' => array('html'))
+            ),
+            'googlePlusButton' => new \Twig_Function_Method(
+                $this,
+                'getGooglePlusButton',
+                array('is_safe' => array('html'))
+            ),
+        );
     }
 
+    /**
+     * Get social buttons
+     *
+     * @param array $parameters Buttons parameters
+     *
+     * @return string
+     */
     public function getSocialButtons($parameters = array())
     {
-      // no parameters were defined, keeps default values
-      if (!array_key_exists('facebook', $parameters)){
-        $render_parameters['facebook'] = array();
-      // parameters are defined, overrides default values
-      }else if(is_array($parameters['facebook'])){
-        $render_parameters['facebook'] = $parameters['facebook'];
-      // the button is not displayed 
-      }else{
-        $render_parameters['facebook'] = false;
-      }
+        // no parameters were defined, keeps default values
+        if ( !array_key_exists('facebook', $parameters) ) {
+            $render_parameters['facebook'] = array();
+            // parameters are defined, overrides default values
+        } else if ( is_array($parameters['facebook']) ) {
+            $render_parameters['facebook'] = $parameters['facebook'];
+            // the button is not displayed 
+        } else {
+            $render_parameters['facebook'] = false;
+        }
 
-      if (!array_key_exists('twitter', $parameters)){
-        $render_parameters['twitter'] = array();
-      }else if(is_array($parameters['twitter'])){
-        $render_parameters['twitter'] = $parameters['twitter'];
-      }else{
-        $render_parameters['twitter'] = false;
-      }
+        if ( !array_key_exists('twitter', $parameters) ) {
+            $render_parameters['twitter'] = array();
+        } else if ( is_array($parameters['twitter']) ) {
+            $render_parameters['twitter'] = $parameters['twitter'];
+        } else {
+            $render_parameters['twitter'] = false;
+        }
 
-      if (!array_key_exists('googleplus', $parameters)){
-        $render_parameters['googleplus'] = array();
-      }else if(is_array($parameters['googleplus'])){
-        $render_parameters['googleplus'] = $parameters['googleplus'];
-      }else{
-        $render_parameters['googleplus'] = false;
-      }
+        if ( !array_key_exists('googleplus', $parameters) ) {
+            $render_parameters['googleplus'] = array();
+        } else if ( is_array($parameters['googleplus']) ) {
+            $render_parameters['googleplus'] = $parameters['googleplus'];
+        } else {
+            $render_parameters['googleplus'] = false;
+        }
 
-      // get the helper service and display the template
-      return $this->container->get('bach.socialBarHelper')->socialButtons($render_parameters);
+        // get the helper service and display the template
+        return $this->container->get('bach.socialBarHelper')
+            ->socialButtons($render_parameters);
     }
- 
-    // https://developers.facebook.com/docs/reference/plugins/like/ 
+
+    /**
+     * Facebook like button
+     *
+     * @param array $parameters Fb like parameters
+     *
+     * @see https://developers.facebook.com/docs/reference/plugins/like/
+     *
+     * @return string
+     */
     public function getFacebookLikeButton($parameters = array())
     {
-       // default values, you can override the values by setting them
-       $parameters = $parameters + array(
+        // default values, you can override the values by setting them
+        $parameters = $parameters + array(
             'url' => null,
             /*'locale' => 'en_US',*/
             'locale' => 'fr_FR',
@@ -78,12 +143,20 @@ class BachSocialBar extends \Twig_Extension{
             'layout' => 'button_count',
         );
 
-       return $this->container->get('bach.socialBarHelper')->facebookButton($parameters);
+        return $this->container->get('bach.socialBarHelper')
+            ->facebookButton($parameters);
     }
 
+    /**
+     * Twitter buttons
+     *
+     * @param array $parameters Twitter parameters
+     *
+     * @return string
+     */
     public function getTwitterButton($parameters = array())
     {
-       $parameters = $parameters + array(
+        $parameters = $parameters + array(
             'url' => null,
             /*'locale' => 'en',*/
             'locale' => 'fr',
@@ -95,13 +168,20 @@ class BachSocialBar extends \Twig_Extension{
             'tag' => '#bach',
         );
 
-
-       return $this->container->get('bach.socialBarHelper')->twitterButton($parameters);
+        return $this->container->get('bach.socialBarHelper')
+            ->twitterButton($parameters);
     }
 
+    /**
+     * Google Plus button
+     *
+     * @param array $parameters G+ parameters
+     *
+     * @return string
+     */
     public function getGooglePlusButton($parameters = array())
     {
-       $parameters = $parameters + array(
+        $parameters = $parameters + array(
             'url' => null,
             /*'locale' => 'en',*/
             'locale' => 'fr',
@@ -110,6 +190,7 @@ class BachSocialBar extends \Twig_Extension{
             'width' => '300',
         );
 
-       return $this->container->get('bach.socialBarHelper')->googlePlusButton($parameters);
+        return $this->container->get('bach.socialBarHelper')
+            ->googlePlusButton($parameters);
     }
 }
